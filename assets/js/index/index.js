e3b866decbacbac0e5ac3330e5ad2fba693fa4f0
meganoob <kasrahabibbeygi@gmail.com>
2020-11-12 23:50:50 +0330

    fix index header

--- assets/js/index/index.js
+++ assets/js/index/index.js
@@ -1,10 +1,18 @@
 // header big slider 
 
 var swiper = new Swiper('.HRBS', {
-    loop : true,
-    effect: 'fade',
-    navigation: {
-      nextEl: '.RHS_next_arrow',
-      prevEl: '.RHS_prev_arrow',
-    },
-  });
+  loop: true,
+  effect: 'fade',
+  navigation: {
+    nextEl: '.RHS_next_arrow',
+    prevEl: '.RHS_prev_arrow',
+  },
+});
+
+var swiper = new Swiper('.HLLS', {
+  loop: true,
+  navigation: {
+    nextEl: '.LHS_next_arrow',
+    prevEl: '.LHS_right_arrow',
+  },
+});
