49333a40c36d214738ff7db3d7a605e1b4b27e46
meganoob <kasrahabibbeygi@gmail.com>
2020-11-12 18:22:49 +0330

    add img

---
+++ assets/js/index/index.js
@@ -0,0 +1,10 @@
+// header big slider 
+
+var swiper = new Swiper('.HRBS', {
+    loop : true,
+    effect: 'fade',
+    navigation: {
+      nextEl: '.RHS_next_arrow',
+      prevEl: '.RHS_prev_arrow',
+    },
+  });
