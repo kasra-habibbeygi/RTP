ca74b92915e4c75f46ce691569af82f19823b192
Asus <nadi.codes@gmail.com>
2020-11-14 08:40:28 +0330

    editjs

--- assets/js/about-us/about-us.js
+++ assets/js/about-us/about-us.js
@@ -2,19 +2,23 @@
 
 $(document).ready(function (){
     function counter(clas, start, end, duration) {
-        let obj = $(clas),
+        let obj = $(clas).text(),
             current = start,
             range = end - start,
             increment = end > start ? 1 : -1,
             step = Math.abs(Math.floor(duration / range)),
-            timer = setInterval(() => {
+            timer = setInterval(function ()  {
                 current += increment;
-                obj.text = current;
+                $(clas).text(current)
                 if (current === end) {
                     clearInterval(timer);
                 }
             }, step);
     }
+    let count = $('.counter');
+    for (let i=0; i<count.length ; i++){
+        let endCounter=count.eq(i).text();
+        counter(".counter", 0, endCounter, 3000);
+    }
 
-    counter(".counter", 0, 300, 3000);
 });
