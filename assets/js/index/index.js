3bf10b29ca75a8672b70ca373025541ba875b6ad
meganoob <kasrahabibbeygi@gmail.com>
2020-11-14 16:10:51 +0330

    fix some responsive part

--- assets/js/index/index.js
+++ assets/js/index/index.js
@@ -16,8 +16,6 @@ var swiper = new Swiper('.HRBS', {
 
 // product slider 1
 var swiper = new Swiper('.SP000', {
-
-  speed: 1200,
   navigation: {
 
     nextEl: '.PS000_next_arrow',
@@ -30,26 +28,31 @@ var swiper = new Swiper('.SP000', {
       slidesPerView: 1,
       spaceBetween: 11,
       slidesPerGroup: 1,
+      speed: 300,
     },
     500: {
       slidesPerView: 2,
       spaceBetween: 11,
       slidesPerGroup: 1,
+      speed: 400,
     },
     800: {
       slidesPerView: 3,
       spaceBetween: 11,
       slidesPerGroup: 2,
+      speed: 700,
     },
     1110: {
       slidesPerView: 4,
       spaceBetween: 11,
       slidesPerGroup: 3,
+      speed: 1000,
     },
     1280:{
       slidesPerView: 5,
       spaceBetween: 11,
       slidesPerGroup: 4,
+      speed: 1200,
     },
   }
 });
