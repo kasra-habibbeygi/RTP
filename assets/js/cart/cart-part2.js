0e0e915d779c72662841620a7e529b8ac1075542
meganoob <kasrahabibbeygi@gmail.com>
2020-11-29 14:03:31 +0330

    fix post in cart3

--- assets/js/cart/cart-part2.js
+++ assets/js/cart/cart-part2.js
@@ -1,20 +1,20 @@
 // open add address modal
-$('.new_address div').click(function() {
+$('.new_address div').click(function () {
 
     $('.AA_moda').fadeIn();
-    $('body').css('overflow' , 'hidden');
+    $('body').css('overflow', 'hidden');
 
-    $('.AAM_close').click(function(){
+    $('.AAM_close').click(function () {
 
         $('.AA_moda').fadeOut();
-        $('body').css('overflow' , 'auto');
+        $('body').css('overflow', 'auto');
 
     })
 
 });
 
 // chose day zone
-$('.send_day .PD_chose_box div').click(function(){
+$('.send_day .PD_chose_box div').click(function () {
 
     $('.send_day .PD_chose_box div').removeClass('active_pill');
     $(this).addClass('active_pill');
@@ -22,9 +22,29 @@ $('.send_day .PD_chose_box div').click(function(){
 });
 
 // chose time zone
-$('.send_time .PD_chose_box div').click(function(){
+$('.send_time .PD_chose_box div').click(function () {
 
     $('.send_time .PD_chose_box div').removeClass('active_pill');
     $(this).addClass('active_pill');
+
+});
+
+// when user chose express , display field of time and user can chose one of them
+// when user chose post , do nothing
+
+
+$('.inp-cbx').change(function () {
+
+    if ($('#PM-1').is(":checked")) {
+
+        $('.time_field').slideDown()
+
+    }
+
+    if($('#PM-2').is(':checked')){
+
+        $('.time_field').slideUp();
+
+    }
 
 });
