b9ef0d6d77067d190d64ac4ada29ffb82083fbf3
meganoob <kasrahabibbeygi@gmail.com>
2020-12-30 22:35:30 +0330

    fix some bugs

--- assets/js/products/product-list.js
+++ assets/js/products/product-list.js
@@ -1,12 +1,12 @@
-$('#OPF').click(function(){
+$('#OPF').click(function () {
 
     $('.main_filter_field').fadeIn();
-    $('body').css('overflow' , 'hidden');
+    $('body').css('overflow', 'hidden');
 
-    $('.close_FF').click(function(){
+    $('.close_FF').click(function () {
 
         $('.main_filter_field').fadeOut();
-        $('body').css('overflow' , 'auto');
+        $('body').css('overflow', 'auto');
 
     });
 
@@ -14,12 +14,12 @@ $('#OPF').click(function(){
 
 // if user in mobile size open and close filter and then resize browser to higher than 1050
 // the filter display is none , blow code fix this
-$(window).resize(function() {
+$(window).resize(function () {
 
-    if($(window).width() > 1050){
+    if ($(window).width() > 1050) {
 
         $('.main_filter_field').fadeIn();
-    
+
     }
 
 });
@@ -29,17 +29,24 @@ $(document).scroll(function () {
 
     var scroll = $(this).scrollTop();
 
-    if($(window).width() < 1050){
+    if ($(window).width() < 1050) {
 
         if (scroll > 80) {
 
             $('.filter_navbar').addClass('sticky_filter');
-    
+
         } else {
-    
+
             $('.filter_navbar').removeClass('sticky_filter');
-    
+
         }
 
     }
+});
+
+
+$('.pagination_ul .enable').click(function (event) {
+
+    event.preventDefault();
+
 });
