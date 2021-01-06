1a381140b57cff72813a270d1a97d95e02e4b26e
meganoob <kasrahabibbeygi@gmail.com>
2021-01-06 14:14:36 +0330

    fix some bugs

--- assets/js/aricles/articles-details.js
+++ assets/js/aricles/articles-details.js
@@ -78,3 +78,27 @@ $(document).scroll(function () {
     }
 
 });
+
+$('.check_valid').click(function (e){
+
+    let inp_count = $('form div .valid').length;
+    let inp_val = [];
+    let error_text = $('.error_handler .error_content p');
+    
+    // get input value and push them into the inp_val array
+    for(let i = 0 ; i < inp_count ; i++){
+    
+        inp_val.push($('form div .valid').eq(i).val());
+    
+    }
+    
+    // check if inp is empty
+    if(inp_val[0] == '' || inp_val[1] == '' || inp_val[2] == ''){
+
+        error_text.text('لطفا تمام فیلد ها را پر کنید .');
+        $('.error_handler').css('display' , 'flex');
+        e.preventDefault();
+        
+    }
+
+});
