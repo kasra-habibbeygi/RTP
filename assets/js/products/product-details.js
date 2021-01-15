e57bfcb5961f26ec238d516f9c9861270e7e3e30
meganoob <kasrahabibbeygi@gmail.com>
2021-01-15 15:13:32 +0330

    creat errors in product detaild

--- assets/js/products/product-details.js
+++ assets/js/products/product-details.js
@@ -163,7 +163,7 @@ $('.check_valid').click(function (e) {
 // when user chose memory , color change
 for (let x = 0; x <= $('.memory_chose .PDCB_parent div').length; x++) {
 
-    $('.memory_chose .PDCB_parent div').eq(x).click(function(){
+    $('.memory_chose .PDCB_parent div').eq(x).click(function () {
 
         $('.color_chose .PDCB_parent div').removeClass('active_pill');
         $('.color_chose .color_pill').removeClass('active_by_memory');
@@ -173,23 +173,45 @@ for (let x = 0; x <= $('.memory_chose .PDCB_parent div').length; x++) {
 
 }
 
-$('.add_to_basket').click(function(){
+// when user click on 'add to card btn' , errors show
+$('.add_to_basket').click(function () {
 
-    if($('.color_chose .PDCB_parent div').hasClass('active_pill') == false){
+    // if user dont chose color , error text show
+    if ($('.color_chose .PDCB_parent div').hasClass('active_pill') == false) {
 
         $('.flow_error_field P').text('ابتدا رنگ مورد نظر خود را انتخاب کنید .');
-        $('.flow_error_field').css('right' , '20px');
-    }else{
+        $('.flow_error_field').css('right', '20px');
+
+        // if goods add to card , done text showed
+    } else {
 
         let cardCount = $('.B-conter').text();
         $('.B-conter').text(parseInt(cardCount) + 1);
+        $('.flow_error_field').css('right', '-500px');
+
+
+        setTimeout(function () {
+
+            $('.flow_error_field P').text('محصول شما به سبد خرید اضافه شد .');
+            $('.flow_error_field').addClass('add_done')
+            $('.flow_error_field').css('right', '20px');
+
+        }, 600);
 
     }
 
-    $('.remove_FEF').click(function(){
+    // when user click on exit on error field , error field close
+    $('.remove_FEF').click(function () {
 
-        $('.flow_error_field').css('right' , '-500px');
+        $('.flow_error_field').css('right', '-500px');
 
-    })
+
+    });
+
+    setTimeout(function(){
+
+        $('.flow_error_field').css('right', '-500px');
+
+    },5000)
 
 });
