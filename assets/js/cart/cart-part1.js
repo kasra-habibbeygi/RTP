a9c4e976dad47bff6096d83547c4772e573be843
meganoob <kasrahabibbeygi@gmail.com>
2021-01-17 11:08:20 +0330

    fix js bugs

--- assets/js/cart/cart-part1.js
+++ assets/js/cart/cart-part1.js
@@ -69,8 +69,10 @@ $RG.click(function(){
     var data_title = $(this).attr('data-title');
     var data_memory = $(this).attr('data-memory');
     var data_color = $(this).attr('data-color');
+    var data_url = $(this).attr('data-url');
 
     $('.confiem_content p').text(`ایا مطمئن هستید که میخواهید ${data_title} با حافظه داخلی ${data_memory} و رنگ ${data_color} از سبد خرید شما حذف شود ؟`);
 
+    $('#CC_yes').attr('href' , data_url);
 
 })
