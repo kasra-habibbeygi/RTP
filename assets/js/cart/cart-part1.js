d56c758898f019df74ae8002bdeb46bdc4ad8c53
meganoob <kasrahabibbeygi@gmail.com>
2021-01-17 13:06:00 +0330

    sadf

--- assets/js/cart/cart-part1.js
+++ assets/js/cart/cart-part1.js
@@ -71,7 +71,17 @@ $RG.click(function(){
     var data_color = $(this).attr('data-color');
     var data_url = $(this).attr('data-url');
 
-    $('.confiem_content p').text(`ایا مطمئن هستید که میخواهید ${data_title} با حافظه داخلی ${data_memory} و رنگ ${data_color} از سبد خرید شما حذف شود ؟`);
+    if(data_memory == ''){
+
+        $('.confiem_content p').text(`ایا مطمئن هستید که میخواهید ${data_title} رنگ ${data_color} از سبد خرید شما حذف شود ؟`);
+
+    }else{
+
+
+        $('.confiem_content p').text(`ایا مطمئن هستید که میخواهید ${data_title} با حافظه داخلی ${data_memory} و رنگ ${data_color} از سبد خرید شما حذف شود ؟`);
+
+    }
+
 
     $('#CC_yes').attr('href' , data_url);
 
