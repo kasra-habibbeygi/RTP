db6f4f1ae7054da7ca1c9d32612f44ab7d0b3b76
meganoob <kasrahabibbeygi@gmail.com>
2021-01-22 13:03:08 +0330

    remove validation

--- assets/js/products/product-details.js
+++ assets/js/products/product-details.js
@@ -151,30 +151,7 @@ $('.memory_chose .PDCB_parent div').click(function () {
 
 });
 
-// form validations
 
-$('.check_valid').click(function (e) {
-
-    let inp_count = $('form div .valid').length;
-    let inp_val = [];
-    let error_text = $('.error_handler .error_content p');
-
-    // get input value and push them into the inp_val array
-    for (let i = 0; i < inp_count; i++) {
-
-        inp_val.push($('form div .valid').eq(i).val());
-
-    }
-
-    // check if inp is empty
-    if (inp_val[0] == '' || inp_val[1] == '' || inp_val[2] == '') {
-
-        error_text.text('لطفا تمام فیلد ها را پر کنید .');
-        $('.error_handler').css('display', 'flex');
-        e.preventDefault();
-    }
-
-});
 
 // when user chose memory , color change
 for (let x = 0; x <= $('.memory_chose .PDCB_parent div').length; x++) {
