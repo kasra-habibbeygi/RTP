587e5f855d759b23f332a0501123409b98c52d93
meganoob <kasrahabibbeygi@gmail.com>
2021-01-22 10:05:43 +0330

    fix some bugs

--- assets/js/profile/address-list.js
+++ assets/js/profile/address-list.js
@@ -28,6 +28,10 @@ $('.remove_tr').click(function(){
 
     $('.RR_modal').fadeIn();
 
+    let dataUrl = $(this).attr('data-url');
+
+    $('.RR_modal .main_field .yes').attr('href' , dataUrl);
+
     $('.RR_modal .no').click(function(){
 
         $('.RR_modal').fadeOut();
